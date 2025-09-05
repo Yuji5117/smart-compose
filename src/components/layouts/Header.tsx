d579491656fb4c19dfa64d7ee5a6523c17e5580b
Yuji Nakamura <ny5117@icloud.com>
2025-09-05 12:27:04 +1000

    HeaderコンポーネントのUIを実装

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-between border-b border-gray-200 px-9 py-4 shadow-sm">
+      <h1 className="text-lg">Smart Composer</h1>
+      <button className="bg-primary rounded-md py-1 px-2 text-sm text-white">
+        新しい原稿を作成
+      </button>
+    </header>
+  );
 };
