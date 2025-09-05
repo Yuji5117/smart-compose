f138d5c7a4754f3b06382e8357bee9b656671c97
Yuji Nakamura <ny5117@icloud.com>
2025-09-05 12:30:02 +1000

    buttonにポインターとホバー時にopacityをつける

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header className="flex justify-between border-b border-gray-200 px-9 py-4 shadow-sm">
       <h1 className="text-lg">Smart Composer</h1>
-      <button className="bg-primary rounded-md py-1 px-2 text-sm text-white">
+      <button className="bg-primary cursor-pointer rounded-md px-2 py-1 text-sm text-white hover:bg-primary/80">
         新しい原稿を作成
       </button>
     </header>
