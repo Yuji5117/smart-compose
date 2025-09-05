7871a065e9f8ac878011620ccbb000f72af7b8c4
Yuji Nakamura <ny5117@icloud.com>
2025-09-05 12:38:28 +1000

    コンフリクト解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div className="">Smart Compose</div>;
+import { JobInputPanel } from '@/feature/job-posting/components/input/JobInputPanel';
+
+export default function JobCreatorPage() {
+  return (
+    <main className="container mx-auto px-4">
+      <div className="grid">
+        <JobInputPanel />
+      </div>
+    </main>
+  );
 }
