7e4d70c0e9eb56774c56d8735db285bfd7ad7c1e
Yuji Nakamura <ny5117@icloud.com>
2025-09-07 15:33:29 +0900

    EditorPanelをpageに表示させるように宣言 また、cssの微修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { EditorPanel } from '@/feature/job-posting/components/editer/EditorPanel';
 import { JobInputPanel } from '@/feature/job-posting/components/input/JobInputPanel';
 
 export default function JobCreatorPage() {
   return (
     <main className="container mx-auto px-4 pt-6">
-      <div className="grid">
+      <div className="flex flex-row gap-x-6">
         <JobInputPanel />
+        <EditorPanel />
       </div>
     </main>
   );
